a3df8fa569dae2fbf8222526925309756816bda4
abed1414 <abedkansof18@gmail.com>
2024-09-27 07:07:55 -0400

    Second Commit check

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,14 +1,15 @@
-/*jshint esversion: 8 */ 
+/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const { loadData } = require("./util/import-mongo/index");
+const {loadData} = require("./util/import-mongo/index");
+
 
 const app = express();
-app.use("*", cors()); // Middleware for handling CORS
+app.use("*",cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
@@ -17,28 +18,20 @@ connectToDatabase().then(() => {
 })
     .catch((e) => console.error('Failed to connect to DB', e));
 
-app.use(express.json()); // Middleware for parsing JSON bodies
+
+app.use(express.json());
 
 // Route files
-
-// Gift API Task 1: Import the giftRoutes and store in a constant called giftRoutes
-const giftRoutes = require('./routes/giftRoutes'); // Importing gift routes
-
-// Search API Task 1: Import the searchRoutes and store in a constant called searchRoutes
-const searchRoutes = require('./routes/searchRoutes'); // Importing search routes
-
+const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
-app.use(pinoHttp({ logger })); // Middleware for logging HTTP requests
+app.use(pinoHttp({ logger }));
 
 // Use Routes
-
-// Gift API Task 2: Add the giftRoutes to the server by using the app.use() method
-app.use('/api/gifts', giftRoutes); // Attaching gift routes to /api/gifts
-
-// Search API Task 2: Add the searchRoutes to the server by using the app.use() method
-app.use('/api/search', searchRoutes); // Attaching search routes to /api/search
+app.use('/api/gifts', giftRoutes);
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
@@ -46,12 +39,10 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-// Default route for root
-app.get("/", (req, res) => {
-    res.send("Inside the server");
-});
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
 
-// Start the server and listen on the defined port
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
